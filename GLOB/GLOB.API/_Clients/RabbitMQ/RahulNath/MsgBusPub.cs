676b1258c07ebd2bb2b8928acd8d0d276cdaeb75
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-08-29 14:54:42 +0500

    GO

---
+++ GLOB/GLOB.API/_Clients/RabbitMQ/RahulNath/MsgBusPub.cs
@@ -0,0 +1,95 @@
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace GLOB.API.Clientz;
+public class MsgBusPubFactory : IDisposable
+{
+  protected readonly Option_RabbitMQ _option_RabbitMQ;
+  protected IConnection _connection;
+  protected IModel _channel;
+  public MsgBusPubFactory(IServiceProvider sp)
+  {
+    _option_RabbitMQ = sp.GetSrvc<IOptions<Option_App>>().Value.Clients.RabbitMQz;
+    Init();
+  }
+  public void Publish(object data)
+  {
+    try
+    {
+      var message = JsonConvert.SerializeObject(data);
+      if (_connection.IsOpen)
+      {
+        "Connection Open, sending message...".Print("Rabbit MQ");;
+        SendMessage(message);
+      }
+      else
+      {
+        "Connection Close, not sending".Print("Rabbit MQ");
+      }
+    }
+    catch (Exception ex)
+    {
+      $"Serialization failed: {ex.Message}".Print("Rabbit MQ");
+      ex.StackTrace.Print();
+    }
+  }
+  private void SendMessage(string message)
+  {
+    var body = Encoding.UTF8.GetBytes(message);
+    _channel.BasicPublish(
+      exchange: "trigger",
+      routingKey: "",
+      basicProperties: null,
+      body
+    );
+    $"--> We have send: {message}".Print("Rabbit MQ");;
+  }
+  public void Init()
+  {
+    var factory = new ConnectionFactory
+    {
+      HostName = _option_RabbitMQ.HostName,
+      Port = _option_RabbitMQ.Port,
+      // UserName = _option_RabbitMQ.UserName,
+      // Password = _option_RabbitMQ.Password
+    };
+    try
+    {
+      _connection = factory.CreateConnection();
+      _channel = _connection.CreateModel();
+
+      _channel.ExchangeDeclare(
+          exchange: "trigger",
+          type: ExchangeType.Fanout
+      );
+      _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+      "Connection Successfull".Print("Rabbit MQ");;
+    }
+    catch (Exception ex)
+    {
+      $"Connection Failed{ex.Message}".Print("Rabbit MQ");;
+    }
+  }
+
+  private void RabbitMQ_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+  {
+    "connection was shut down. Jackson".Print("Rabbit MQ");
+  }
+  
+  public void Dispose()
+  {
+    if (_channel != null && _channel.IsOpen)
+    {
+      _channel.Close();
+      _channel.Dispose();
+    }
+
+    if (_connection != null && _connection.IsOpen)
+    {
+      _connection.Close();
+      _connection.Dispose();
+    }
+  }
+}
---
+++ GLOB/GLOB.API/_Clients/RabbitMQ/RahulNath/MsgBusSubs.cs
@@ -0,0 +1,120 @@
+using System.Text;
+
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace GLOB.API.Clientz;
+
+public class MsgBusSubFactory: BackgroundService
+{
+  private readonly IConfiguration _config;
+  private readonly IServiceScopeFactory _scopeFactory;
+  private readonly Option_RabbitMQ _option_RabbitMQ;
+  private IConnection _connection;
+  private IModel _channel;
+  private string _queueName;
+
+  public MsgBusSubFactory(IServiceProvider sp)
+  {
+    _config = sp.GetSrvc<IConfiguration>();
+    _scopeFactory = sp.GetSrvc<IServiceScopeFactory>();
+    _option_RabbitMQ = sp.GetSrvc<IOptions<Option_App>>().Value.Clients.RabbitMQz;
+    InitRabbitMQ();
+  }
+
+  private void InitRabbitMQ()
+  {
+    var factory = new ConnectionFactory()
+    {
+      HostName = _option_RabbitMQ.HostName,
+      Port = _option_RabbitMQ.Port,
+    };
+
+    _connection = factory.CreateConnection();
+    _channel = _connection.CreateModel();
+    _channel.ExchangeDeclare(
+      exchange: "trigger",
+      type: ExchangeType.Fanout
+    );
+
+    _queueName = _channel.QueueDeclare().QueueName;
+
+    _channel.QueueBind(
+      queue: _queueName,
+      exchange: "trigger",
+      routingKey: ""
+    );
+    _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+    "Listening on the Message Bus...".Print("Rabbit MQ");
+  }
+
+  protected override Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    stoppingToken.ThrowIfCancellationRequested();
+    var consumer = new EventingBasicConsumer(_channel);
+    consumer.Received += async (ModuleHandle, ea) =>
+    {
+      "Message Recieved".Print("Rabbit MQ");
+      var body = ea.Body;
+      var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+      await ProcessEvent(notificationMessage);
+
+    };
+    _channel.BasicConsume(
+      queue: _queueName,
+      autoAck: true,
+      consumer: consumer
+    );
+    return Task.CompletedTask;
+  }
+
+  public async Task ProcessEvent(string message)
+  {
+    try
+    {
+      using var scope = _scopeFactory.CreateScope();
+      using var uow = scope.ServiceProvider.GetRequiredService<IUOW_Infra>();
+      var model = JsonConvert.DeserializeObject<ProjectzLookup>(message);
+
+      if (uow.ProjectzLookupBases.AnyId(model?.ProjectzLookupBaseId ?? 0))
+      {
+        await uow.ProjectzLookups.Add(model);
+        await uow.Save();
+        "ProjectzLookup Created Successfully".Print();
+      }
+      else
+      {
+        "ProjectzLookupBaseId Does not Exsist".Print();
+      }
+
+    }
+    catch (Exception ex)
+    {
+      $"ProjectzLookup not Created {ex.Message}".Print();
+    }
+  }
+
+  private void RabbitMQ_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+  {
+    "connection was shut down. Jackson".Print("Rabbit MQ");
+  }
+  public override void Dispose()
+  {
+    if (_channel != null && _channel.IsOpen)
+    {
+      _channel.Close();
+      _channel.Dispose();
+    }
+
+    if (_connection != null && _connection.IsOpen)
+    {
+      _connection.Close();
+      _connection.Dispose();
+    }
+    base.Dispose();
+  }
+}
