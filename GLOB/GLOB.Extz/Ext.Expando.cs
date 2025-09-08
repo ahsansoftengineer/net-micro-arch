d47e77762196a2db7230209e6aacb1267105b91d
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-09-08 12:54:51 +0500

    GO

---
+++ GLOB/GLOB.Extz/Ext.Expando.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace GLOB.Extz;
+
+public static partial class Exts
+{
+  public static ExpandoObject ToExpando(this object obj)
+  {
+    if (obj == null)
+      return null;
+
+    IDictionary<string, object> expando = new ExpandoObject();
+
+    var properties = obj.GetType().GetProperties();
+    foreach (var prop in properties)
+    {
+      expando[prop.Name] = prop.GetValue(obj);
+    }
+
+    return (ExpandoObject)expando;
+  }
+
+  public static ExpandoObject ToExpando(this object obj, IDictionary<string, object> extraProps)
+  {
+    var expando = obj.ToExpando();
+
+    if (extraProps != null)
+    {
+      var dict = (IDictionary<string, object>)expando;
+      foreach (var kv in extraProps)
+      {
+        dict[kv.Key] = kv.Value;
+      }
+    }
+
+    return expando;
+  }
+}
