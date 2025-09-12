eb835a681509fae056f1f4148727b4a776c984f3
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-09-12 14:56:22 +0500

    GO

---
+++ Projects/SBA.Hierarchy/_Clients/RMQ/RMQ.Pub.cs
@@ -0,0 +1,36 @@
+using GLOB.API.Clientz;
+using RabbitMQ.Client;
+
+namespace SBA.Projectz.Clientz;
+
+public class Projectz_RMQ_Pub : API_RMQ_Pub
+{
+  public Projectz_RMQ_Pub(IServiceProvider sp) : base(sp)
+  {
+    ExchangeDeclare(null);
+  }
+  protected override void ExchangeDeclare(Action<IModel> action = null)
+  {
+    base.ExchangeDeclare((channel) =>
+    {
+      channel.ExchangeDeclare(
+        exchange: "sba.direct",
+        type: ExchangeType.Direct,
+        durable: true,
+        autoDelete: false
+      );
+      // channel.ExchangeDeclare(
+      //   exchange: "sba.fanout",
+      //   type: ExchangeType.Fanout
+      // );
+      // channel.ExchangeDeclare(
+      //   exchange: "sba.topic",
+      //   type: ExchangeType.Topic
+      // );
+      // channel.ExchangeDeclare(
+      //   exchange: "sba.headers",
+      //   type: ExchangeType.Headers
+      // );
+    });
+  }
+}
---
+++ Projects/SBA.Hierarchy/_Clients/RMQ/RMQ.Sub.cs
@@ -0,0 +1,92 @@
+using System.Text;
+using GLOB.API.Clientz;
+using GLOB.API.Config.Optionz;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SBA.Projectz.Clientz;
+
+public class Projectz_RMQ_Sub : API_RMQ_Sub
+{
+  public Projectz_RMQ_Sub(IServiceProvider sp) : base(sp)
+  {
+    Init("sba", ExchangeType.Direct, "");
+  }
+  protected override void ExchangeDeclare(Action<IModel> action = null)
+  {
+    base.ExchangeDeclare((channel) =>
+    {
+      channel.ExchangeDeclare(
+        exchange: "sba.direct",
+        type: ExchangeType.Direct
+      );
+    });
+  }
+  protected void Init(string exchange = "sba", string exchangeType = ExchangeType.Direct, string route = "route-default")
+  {
+
+    _channel.ExchangeDeclare(
+      exchange: exchange,
+      type: exchangeType
+    );
+
+    _queueName = _channel.QueueDeclare().QueueName;
+
+    _channel.QueueBind(
+      queue: _queueName,
+      exchange: exchange,
+      routingKey: route
+    );
+    // _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+    "Listening on the Message Bus...".Print("Rabbit MQ");
+  }
+
+  protected override Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    stoppingToken.ThrowIfCancellationRequested();
+    var consumer = new EventingBasicConsumer(_channel);
+    consumer.Received += async (ModuleHandle, ea) =>
+    {
+      "Message Recieved".Print("Rabbit MQ");
+      var body = ea.Body;
+      var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+      await ProcessEvent(notificationMessage);
+
+    };
+    _channel.BasicConsume(
+      queue: _queueName,
+      autoAck: true,
+      consumer: consumer
+    );
+    return Task.CompletedTask;
+  }
+  public async Task ProcessEvent(string message)
+  {
+    try
+    {
+      using var scope = _scopeFactory.CreateScope();
+      using var uow = scope.ServiceProvider.GetRequiredService<IUOW_Infra>();
+      var model = JsonConvert.DeserializeObject<ProjectzLookup>(message);
+
+      if (uow.ProjectzLookupBases.AnyId(model?.ProjectzLookupBaseId ?? 0))
+      {
+        await uow.ProjectzLookups.Add(model);
+        await uow.Save();
+        "ProjectzLookup Created Successfully".Print();
+      }
+      else
+      {
+        "ProjectzLookupBaseId Does not Exsist".Print();
+      }
+
+    }
+    catch (Exception ex)
+    {
+      $"ProjectzLookup not Created {ex.Message}".Print();
+    }
+  }
+}
